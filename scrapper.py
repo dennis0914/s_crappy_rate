0da6ea9b5469c29c1562b3a4cf411ef394acc0d6
Dennis Bae <dennis0914@naver.com>
2021-11-10 23:14:37 +0900

    scrap BI news articles

--- scrapper.py
+++ scrapper.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 from requests.models import ContentDecodingError
 import parser
-url = "https://www.marketwatch.com/column/the-fed?mod=side_nav"
-r1 = requests.get(url)
+market_watch_url = "https://www.marketwatch.com/column/the-fed?mod=side_nav"
+r1 = requests.get(market_watch_url)
 coverpage = r1.content
 
 soup = BeautifulSoup(coverpage, 'html.parser')
@@ -13,4 +13,14 @@ headlines_list = list()
 for headline in headlines:
     headlines_list.append(headline.get_text().strip())
 
-print(headlines_list[3])
+business_insider_url = "https://www.businessinsider.com/s?q=fed"
+r = requests.get(business_insider_url).content
+soup = BeautifulSoup(r, 'html.parser')
+headlines = soup.find_all(class_="tout-title-link")
+headlines_list = list()
+for headline in headlines:
+    headlines_list.append(headline.get_text().strip())
+
+for i in headlines_list:
+    print(i)
+    print("\n")
