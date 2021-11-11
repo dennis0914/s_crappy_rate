dd8b90ac7ba5c993f6203e71cfa3d6ed316c2a03
Dennis Bae <dennis0914@naver.com>
2021-11-11 14:12:44 +0900

    scrape yahoo and bing

--- scrapper.py
+++ scrapper.py
@@ -1,26 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 from requests.models import ContentDecodingError
-import parser
-market_watch_url = "https://www.marketwatch.com/column/the-fed?mod=side_nav"
-r1 = requests.get(market_watch_url)
-coverpage = r1.content
 
-soup = BeautifulSoup(coverpage, 'html.parser')
 
-headlines = soup.find_all(class_="article__headline")
-headlines_list = list()
-for headline in headlines:
-    headlines_list.append(headline.get_text().strip())
+def scrape():
+    market_watch = "https://www.marketwatch.com/column/the-fed?mod=side_nav"
+    business_insider = "https://www.businessinsider.com/s?q=fed"
+    yahoo = "https://news.search.yahoo.com/search?p=the+fed&fr=uh3_news_vert_gs&fr2=p%3Anews%2Cm%3Asb"
+    bing = "https://www.bing.com/news/search?q=the+fed&qft=interval%3d%228%22+sortbydate%3d%221%22&form=NWRFSH"
 
-business_insider_url = "https://www.businessinsider.com/s?q=fed"
-r = requests.get(business_insider_url).content
-soup = BeautifulSoup(r, 'html.parser')
-headlines = soup.find_all(class_="tout-title-link")
-headlines_list = list()
-for headline in headlines:
-    headlines_list.append(headline.get_text().strip())
+    headlines_list = list()
 
-for i in headlines_list:
-    print(i)
-    print("\n")
+    scrape_target={
+        bing: {'target': '_blank', 'class': 'title'},
+        market_watch: {'class': 'article__headline'},
+        business_insider: {'class': 'tout-title-link'},
+        yahoo: {'style': 'font-size:16px;', 'referrerpolicy': 'unsafe-url'}
+        }
+
+    for key, value in scrape_target.items():
+        r = requests.get(key, headers = {"Accept-Language": "en-US"}).content
+        soup = BeautifulSoup(r, 'html.parser')
+        headlines = soup.find_all(attrs=value)
+        for headline in headlines:
+            #drop if headline is not in english
+            try:
+                headline.get_text().strip().encode(encoding='utf-8').decode('ascii')
+            except UnicodeDecodeError:
+                continue
+            else:
+                headlines_list.append(headline.get_text().strip())
+
+    return headlines_list
